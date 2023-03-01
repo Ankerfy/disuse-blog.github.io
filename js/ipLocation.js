eda62137c44e9b7aa2db031b2083da9fead63a3c
anker <wgh12102277@163.com>
2023-03-01 19:25:56 +0800

    Site updated: 2023-03-01 19:25:53

--- js/ipLocation.js
+++ js/ipLocation.js
@@ -219,7 +219,7 @@ function showWelcome() {
         document.getElementById("welcome-info").innerHTML =
             `<b><center>👏 欢迎信息 👏</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
     } catch (err) {
-        // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
+        console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
     }
 }
 window.onload = showWelcome;
